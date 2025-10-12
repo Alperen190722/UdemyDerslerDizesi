e67a32370ba2f730dd153a493f1ae58d3428426d
Alperen Pişkin <alperen013@outlook.com.tr>
2025-10-12 12:54:08 +0300

    Metotlar Bitti

---
+++ B07-Metotlar/MatematikIslemler/Matematik.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematikIslemler
+{
+    public class Matematik
+    {
+        //Topla
+        public decimal toplamaIslemi(decimal sayi1, decimal sayi2) 
+        {
+            decimal sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        // Cikart
+
+        public decimal cikartmaIslemi(decimal sayi1, decimal sayi2) 
+        {
+            decimal sonuc = sayi1 - sayi2;
+            return sonuc;
+        }
+
+        // Bolme İşlemi
+
+        public decimal bolmeIslemi(decimal sayi1, decimal sayi2) 
+        {
+            decimal sonuc = sayi1 / sayi2;
+            return sonuc;
+        }
+
+        // Çarpma
+
+        public decimal carpmaIslemi(decimal sayi1, decimal sayi2) 
+        {
+            decimal sonuc = sayi1 * sayi2;
+            return sonuc;
+        }
+
+        public void menuHazirla() 
+        {
+            Console.Clear();
+            Console.WriteLine("*** Menü ***");
+            Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkarma");
+            Console.WriteLine("3 - Çarpma");
+            Console.WriteLine("4 - Bölme");
+            Console.Write("Lütfen değer seçiniz : ");
+        }
+
+        public void sonucEkranaYaz(decimal kullaniciSayi1, decimal kullaniciSayi2, decimal 
+           sonuc, string operators) 
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", kullaniciSayi1, operators, kullaniciSayi2, 
+               sonuc);
+        }
+    }
+}
---
+++ B07-Metotlar/MetotGenelTanim/Ogrenci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotGenelTanim
+{
+    public class Ogrenci
+    {
+        public void selamlaOgenci() 
+        {
+            Console.WriteLine("Merhaba Öğrenci");
+        }
+
+        /*
+         * 
+         * [ErişimBelirleyicisi] <GeriDonusDegeri> MetotAdi (Parametreler)
+         * {
+         *     // Metot için gereken kodlar...
+         * }
+         * 
+         * Erişim Belirleyicisi
+         * 
+         * Public
+         * Private
+         * Protected
+         * Internal
+         * Internal Protected
+         * 
+         */
+
+        private void OgrenciMetot1(string ogrenciAdi, string ogrneciSoyadi) 
+        {
+            Console.WriteLine("Öğrenci Bilgileri : {0} {1}",ogrenciAdi, ogrneciSoyadi);
+        }
+
+        public void OgrenciMetot2() 
+        {
+            OgrenciMetot1("Alperen", "Pişkin");
+        }
+    }
+}
