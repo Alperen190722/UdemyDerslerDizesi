0a1d63ccffd460c12f56af227ecbecdff5d23e08
Alperen Pişkin <alperen013@outlook.com.tr>
2025-10-17 13:37:14 +0300

    OOP-Kapsülleme Bitti

---
+++ B09-OOP Kapsülleme/KapsullemeNedir/Musteri.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace KapsullemeNedir
+{
+    internal class Musteri
+    {
+        public Musteri()
+        {
+            this.id = IDUret();
+        }
+
+        // Class => Field
+        int id;
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+            private set
+            {
+                this.id = value;
+            }
+        }
+
+        public string isim;
+        private string _soyisim;
+        public string Soyisim
+        {
+            get { return this._soyisim; }
+            set
+            {
+                this._soyisim = value;
+                this.emailAdres = string.Format("{0}.{1}@hotmail.com", isim, _soyisim);
+            }
+        }
+
+
+
+        // Class => property
+        private string emailAdres;
+        public string EmailAdres
+        {
+            // get; // database veya farklı bir veri kaynağı .. class çağıran
+            // programcıya datayı gösterildiğimiz kısım...
+            //set; // dış dünyadan alınan datanın içeride private olarak saklanan
+            // field içerisine değer atandığı kısım..
+
+            private set
+            {
+                this.emailAdres = value;
+            }
+
+            get
+            {
+                return this.emailAdres;
+            }
+        }
+
+        // Ara ödev :
+
+        private string _tckimliknumarasi;
+        public string TCKimlikNumarasi
+        {
+            get { return _tckimliknumarasi.Substring(0, 3); }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    bool bayrak = false;
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool karakterKontrol = char.IsNumber(value[i]);
+                        if (karakterKontrol)
+                        {
+                            // sayısal değer demektir...
+                        }
+                        else
+                        {
+                            bayrak = true;
+                            break;
+                        }
+                    }
+
+                    if (bayrak)
+                    {
+                        Console.WriteLine("TC Kimlik Numarası değerler sayısal olmalıdır...");
+                    }
+                    else
+                    {
+                        this._tckimliknumarasi = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("TC Kimlik Numarası 11 hane olmalıdır...");
+                }
+            }
+        }
+
+        private int IDUret()
+        {
+            Random Rnd = new Random();
+            return Rnd.Next(10000, 90000);
+        }
+    }
+}
---
+++ B09-OOP Kapsülleme/KapsullemeNedir/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapsullemeNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri M1 = new Musteri();
+            /*
+             * Field değer ataması yapılamasın ama değer okunabilsin.
+             * Field değer ataması yapılsın ama değer okumasın.
+             * Field değer ataması yapılsın ama değerin ilk 4 hanesi okunsun.
+             * Field değer ataması ve okuma işlemleri yapılsın ama bu iki işleme bizim 
+             * belirlediğimiz bir süreçten geçsin veri daha sonra gösterilsin.
+             */
+
+            // Class
+            // Field
+            // Property
+            // Get & Set
+
+            // M1.emailAdres = "alperen.piskin@hotmail.com"; ( Field )
+
+            // Genel olarak kapsülleme konusuna giriş yaptık.
+            //M1.EmailAdres = "alperen.piskin@hotmail.com";
+            //Console.Write(M1.EmailAdres);
+
+            //1.Field değer ataması yapılamasın ama değer okunabilsin.
+
+            Console.WriteLine("Musteri Id Değeri = " + M1.ID.ToString());
+            // M1.ID = 10;
+
+            M1.TCKimlikNumarasi = "12254402599";
+            string TC = M1.TCKimlikNumarasi;
+
+            // isim ve soyisim
+            //isim ve soyisim [soyisim] = set
+            // email adresi ....
+            // email get alperen.pişkin@hotmail.com
+            // set : private
+
+            M1.isim = "alperen";
+            M1.Soyisim = "pişkin";
+            Console.WriteLine(M1.EmailAdres);
+        }
+    }
+}
