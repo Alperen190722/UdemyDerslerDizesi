9af7cf87160e7bdc630bfaa5f01e95f9dede8721
Alperen Pişkin <alperen013@outlook.com.tr>
2025-11-01 22:31:52 +0300

    OOP Kalıtım Bitti

---
+++ B12-OOP Kalıtım/KalitimNedir/Ogrenci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalitimNedir
+{
+    public sealed class Ogrenci:temelTip
+    {
+        public string bolum { get; set; }
+        public int Seviye { get; set; }
+
+        public Ogrenci() 
+        {
+            Console.WriteLine("Ogrenci nesnesinin yapıcı metotu çalıştı");
+
+            // TemelTipMetot2(); protected plarak işaretlendiği için ve bu nesne kendisini temeltip
+            // nesnesinden kalıtığı için protected seviyesinde olan field veya metotlara erişilebilir.
+        }
+    }
+}
---
+++ B12-OOP Kalıtım/KalitimOdevUygulamasi/urun.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalitimOdevUygulamasi
+{
+    public class urun:baseClass
+    {
+        public string marka { get; set; } // *belirtmiş olduğumuz markalar içerisinde ürün eklenebilsin
+        public string model { get; set; }
+
+        private decimal _alisFiyat; // 0 dan küçük olmamalı
+        public decimal alisFiyat 
+        {
+            get 
+            { 
+                return this._alisFiyat; 
+            }
+            set 
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Alış fiyatı sıfırdan küçük olamaz");
+                }
+                else 
+                {
+                    _alisFiyat = value;
+                }
+            }
+        }
+        private decimal _satisFiyat; // alisfiyatından küçük olamaz
+        public decimal satisFiyat 
+        { 
+            get 
+            {
+                return this._satisFiyat;
+            }
+            set 
+            {
+                if (value < _alisFiyat)
+                {
+                    Console.WriteLine("Satış fiyatı Alış fiyatından küçük olamaz");
+                }
+                else 
+                { 
+                    _satisFiyat = value;
+                }
+            } 
+        }
+        private decimal _kampanyaFiyat; // kampanya fiyat değeri 0 dan küçük olamaz
+        public decimal kampanyaFiyat 
+        {
+            get 
+            {
+                return this._kampanyaFiyat;
+            }
+            set 
+            {
+                if (value <= 0) 
+                {
+                    Console.WriteLine("Kampanya fiyatı sıfırdan küçük olamaz");
+                }
+                else
+                {
+                    _kampanyaFiyat = value;
+                }
+            }
+        }
+    }
+}
