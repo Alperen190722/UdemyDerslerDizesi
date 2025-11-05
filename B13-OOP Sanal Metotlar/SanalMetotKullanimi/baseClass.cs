b0015cae647d72b12072ae6988b3b18ca50a3d98
Alperen Pişkin <alperen013@outlook.com.tr>
2025-11-05 12:49:11 +0300

    OOP-Sanal Metotlar

---
+++ B13-OOP Sanal Metotlar/SanalMetotKullanimi/baseClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanalMetotKullanimi
+{
+    public class baseClass
+    {
+        public baseClass()
+        {
+            Console.WriteLine("base class");
+        }
+        public virtual void ekranaYaz(string data) 
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
