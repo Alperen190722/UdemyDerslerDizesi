6151853a95a967b64064c347d96edd19f35746b3
Alperen Pişkin <alperen013@outlook.com.tr>
2025-11-07 13:28:54 +0300

    OOP Abstract & Interface

---
+++ B14-OOP Abstract & Interface/AbstractNedir/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            superMusteri SP1 = new superMusteri();
+            SP1.testAbstract();
+
+            // temelSinif T1 = new temelSiniif();
+            Musteri M1 = new Musteri();
+            M1.test();
+            M1.testAbstract();
+        }
+    }
+}
